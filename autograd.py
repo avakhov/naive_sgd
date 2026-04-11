c95f5a605680bbc8f1a257c34e747f6f5dca9c03
Alexey Vakhov <vakhov@gmail.com>
2026-04-11 22:30:10 +0300

    yo

--- autograd.py
+++ autograd.py
@@ -1,96 +1,92 @@
-from __future__ import annotations
-from typing import Callable
-
-
 class Value:
-    def __init__(self, data: float, _prev: set["Value"] = None, _op: str = ""):
+    def __init__(self, data, _prev=None, _op=""):
         self.data = data
         self.grad = 0.0
         self._prev = _prev if _prev is not None else set()
-        self._backward: Callable[[], None] = lambda: None
+        self._backward = lambda: None
         self._op = _op
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
 
-    def __hash__(self) -> int:
+    def __hash__(self):
         return id(self)
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other):
         return self is other
 
     @staticmethod
-    def _ensure_value(x: float | "Value") -> "Value":
+    def _ensure_value(x):
         return x if isinstance(x, Value) else Value(float(x))
 
-    def __add__(self, other: float | "Value") -> "Value":
+    def __add__(self, other):
         other = self._ensure_value(other)
         out = Value(self.data + other.data, _prev={self, other}, _op="+")
 
-        def _backward() -> None:
+        def _backward():
             self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
 
         out._backward = _backward
         return out
 
-    def __radd__(self, other: float | "Value") -> "Value":
+    def __radd__(self, other):
         return self + other
 
-    def __neg__(self) -> "Value":
+    def __neg__(self):
         out = Value(-self.data, _prev={self}, _op="neg")
 
-        def _backward() -> None:
+        def _backward():
             self.grad += -1.0 * out.grad
 
         out._backward = _backward
         return out
 
-    def __sub__(self, other: float | "Value") -> "Value":
+    def __sub__(self, other):
         return self + (-self._ensure_value(other))
 
-    def __rsub__(self, other: float | "Value") -> "Value":
+    def __rsub__(self, other):
         return self._ensure_value(other) + (-self)
 
-    def __mul__(self, other: float | "Value") -> "Value":
+    def __mul__(self, other):
         other = self._ensure_value(other)
         out = Value(self.data * other.data, _prev={self, other}, _op="*")
 
-        def _backward() -> None:
+        def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
 
         out._backward = _backward
         return out
 
-    def __rmul__(self, other: float | "Value") -> "Value":
+    def __rmul__(self, other):
         return self * other
 
-    def __truediv__(self, other: float | "Value") -> "Value":
+    def __truediv__(self, other):
         other = self._ensure_value(other)
         return self * (other ** -1)
 
-    def __rtruediv__(self, other: float | "Value") -> "Value":
+    def __rtruediv__(self, other):
         other = self._ensure_value(other)
         return other * (self ** -1)
 
-    def __pow__(self, power: float) -> "Value":
+    def __pow__(self, power):
         if not isinstance(power, (int, float)):
             raise TypeError("Поддерживаются только int/float степени")
 
         out = Value(self.data ** power, _prev={self}, _op=f"**{power}")
 
-        def _backward() -> None:
+        def _backward():
             self.grad += (power * (self.data ** (power - 1))) * out.grad
 
         out._backward = _backward
         return out
 
-    def backward(self) -> None:
-        topo: list[Value] = []
-        visited: set[Value] = set()
+    def backward(self):
+        topo = []
+        visited = set()
 
-        def build(v: Value) -> None:
+        def build(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
@@ -103,11 +99,11 @@ class Value:
         for node in reversed(topo):
             node._backward()
 
-    def zero_grad(self) -> None:
-        topo: list[Value] = []
-        visited: set[Value] = set()
+    def zero_grad(self):
+        topo = []
+        visited = set()
 
-        def build(v: Value) -> None:
+        def build(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
