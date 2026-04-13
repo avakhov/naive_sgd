80506a83d3ab3474f9159e5c16436980d25913a5
Alexey Vakhov <vakhov@gmail.com>
2026-04-13 15:12:23 +0300

    naive

--- nn.py
+++ nn.py
@@ -24,12 +24,11 @@ def sgd(model, dataset, lr, epochs, batch_size=32, num_snapshots=20, snap_points
             model.snapshots.append((epoch, model.get_graph(t_list)))
 
 class SimpleNN:
-    def __init__(self, n0, n1, n2, n3, sigma=math.tanh):
+    def __init__(self, n0, n1, n2, n3):
         self.n0 = n0
         self.n1 = n1
         self.n2 = n2
         self.n3 = n3
-        self.sigma = sigma
         self.w1 = self._rand_matrix(n0, n1)
         self.b1 = self._rand_array(n1)
         self.w2 = self._rand_matrix(n1, n2)
@@ -37,6 +36,8 @@ class SimpleNN:
         self.w3 = self._rand_matrix(n2, n3)
         self.b3 = self._rand_array(n3)
         self.snapshots = []
+        self.sigma = math.tanh
+        self.deriv = lambda s: 1 - s**2
 
     def forward(self, h0):
         h1 = []
@@ -80,46 +81,91 @@ class SimpleNN:
         dL_b2 = self._zero_array(self.n2)
         dL_w3 = self._zero_matrix(self.n2, self.n3)
         dL_b3 = self._zero_array(self.n3)
-        total_loss = 0.0
+        L = 0.0
         N = len(batch)
         for b in range(N):
             x = [batch[b][i] for i in range(self.n0)]
             y = [batch[b][self.n0 + m] for m in range(self.n3)]
-            # forward, storing pre-activations
-            z1, h1 = [], []
+            # forward
+            z1 = []
+            h1 = []
             for i in range(self.n1):
-                z = sum(x[j] * self.w1[j][i] for j in range(self.n0)) + self.b1[i]
-                z1.append(z); h1.append(self.sigma(z))
-            z2, h2 = [], []
+                z = 0.0
+                for j in range(self.n0):
+                    z += x[j] * self.w1[j][i]
+                z += self.b1[i]
+                z1.append(z)
+                h1.append(self.sigma(z))
+            z2 = []
+            h2 = []
             for i in range(self.n2):
-                z = sum(h1[j] * self.w2[j][i] for j in range(self.n1)) + self.b2[i]
-                z2.append(z); h2.append(self.sigma(z))
-            z3, h3 = [], []
+                z = 0.0
+                for j in range(self.n1):
+                    z += h1[j] * self.w2[j][i]
+                z += self.b2[i]
+                z2.append(z)
+                h2.append(self.sigma(z))
+            z3 = []
+            h3 = []
             for i in range(self.n3):
-                z = sum(h2[j] * self.w3[j][i] for j in range(self.n2)) + self.b3[i]
-                z3.append(z); h3.append(self.sigma(z))
+                z = 0.0
+                for j in range(self.n2):
+                    z += h2[j] * self.w3[j][i]
+                z += self.b3[i]
+                z3.append(z)
+                h3.append(self.sigma(z))
             for m in range(self.n3):
-                total_loss += (h3[m] - y[m]) ** 2
-            # backprop layer 3: sigma=tanh => sigma'(z) = 1 - tanh(z)^2 = 1 - h^2
-            dz3 = [2.0 * (h3[m] - y[m]) / N * (1.0 - h3[m] ** 2) for m in range(self.n3)]
+                L += (h3[m] - y[m]) ** 2
+            # # backprop layer 3
+            dz3 = []
+            for m in range(self.n3):
+                dz3.append(2.0*(h3[m] - y[m])*self.deriv(h3[m]))
             for m in range(self.n3):
                 dL_b3[m] += dz3[m]
                 for j in range(self.n2):
                     dL_w3[j][m] += dz3[m] * h2[j]
             # backprop layer 2
-            dh2 = [sum(dz3[m] * self.w3[j][m] for m in range(self.n3)) for j in range(self.n2)]
-            dz2 = [dh2[i] * (1.0 - h2[i] ** 2) for i in range(self.n2)]
+            dh2 = []
+            for j in range(self.n2):
+                dh2j = 0.0
+                for m in range(self.n3):
+                    dh2j += dz3[m] * self.w3[j][m]
+                d2h.append(dh2j)
+            dz2 = []
+            for i in range(self.n2):
+                dz2.append(dh2[i] * (1.0 - h2[i] ** 2))
             for i in range(self.n2):
                 dL_b2[i] += dz2[i]
                 for j in range(self.n1):
                     dL_w2[j][i] += dz2[i] * h1[j]
             # backprop layer 1
-            dh1 = [sum(dz2[i] * self.w2[j][i] for i in range(self.n2)) for j in range(self.n1)]
-            dz1 = [dh1[k] * (1.0 - h1[k] ** 2) for k in range(self.n1)]
+            dh1 = []
+            for j in range(self.n1):
+                dh1j = 0.0
+                for i in range(self.n2):
+                    dh1j += dz2[i] * self.w2[j][i] 
+                dh1.append(dh1j)
+            dz1 = []
+            for k in range(self.n1):
+                dz1.append(dh1[k] * (1.0 - h1[k] ** 2))
             for k in range(self.n1):
                 dL_b1[k] += dz1[k]
                 for j in range(self.n0):
                     dL_w1[j][k] += dz1[k] * x[j]
+        # avg
+        L /= N
+        for k in range(self.n1):
+            dL_b1[k] /= N
+            for j in range(self.n0):
+                dL_w1[j][k] /= N
+        for i in range(self.n2):
+            dL_b2[i] /= N
+            for j in range(self.n1):
+                dL_w2[j][i] /= N
+        for m in range(self.n3):
+            dL_b3[m] /= N
+            for j in range(self.n2):
+                dL_w3[j][m] /= N
         # SGD update
         for k in range(self.n1):
             self.b1[k] -= lr * dL_b1[k]
@@ -133,7 +179,7 @@ class SimpleNN:
             self.b3[m] -= lr * dL_b3[m]
             for j in range(self.n2):
                 self.w3[j][m] -= lr * dL_w3[j][m]
-        return total_loss / N
+        return L
 
     def get_graph(self, t_list):
         net_x, net_y = [], []
