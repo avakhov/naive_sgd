40203d9d246f6ae7b4cfeecff3868053e5fdc98e
Alexey Vakhov <vakhov@gmail.com>
2026-04-13 18:57:30 +0300

    refac

--- assets/nn.js
+++ assets/nn.js
@@ -1,3 +1,20 @@
+function* sgd(model, dataset, lr, epochs, batch_size = 32, snap_points = 100) {
+    const t_list = Array.from({ length: snap_points + 1 }, (_, i) => i / snap_points);
+    for (let epoch = 0; epoch < epochs; epoch++) {
+        const shuffled = [...dataset];
+        model.random.shuffle(shuffled);
+        let total_loss = 0.0;
+        let batches = 0;
+        for (let i = 0; i < shuffled.length; i += batch_size) {
+            const batch = shuffled.slice(i, i + batch_size);
+            total_loss += model.train(batch, lr);
+            batches++;
+        }
+        const loss = total_loss / batches;
+        yield { epoch, loss };
+    }
+}
+
 class SimpleNN {
     constructor(n0, n1, n2, n3) {
         this.n0 = n0;
